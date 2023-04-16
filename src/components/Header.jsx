2eb6e22dd9f735b2bd0445b5001aef06ee2bdfc6
Daniela De Simone <dsdesimone8@gmail.com>
2023-04-16 12:00:01 -0300

    Change of header in lower media-queries

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,6 +8,7 @@ const Header = () => {
         <img className='header__background' src='./photos/pattern.png' alt=''/>
         <img className='title__name' src="./photos/rm-logo.png" alt="" />
         <img className='title__photo' src="./photos/rm2.jpg" alt="" />
+        <img className='title__photo2' src="./photos/rm1.png" alt="" />
     </header>
   )
 }
